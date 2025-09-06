bed2a03de8b04fc9d7bc84806bccc29618863731
rupini0407 <uppalurivijay91@gmail.com>
2025-09-06 19:11:52 +0530

    Revert "Update service"
    This reverts commit b40f373ab319c4dac36c7ea15dcb6fc82ebd7387.

--- src/main/java/greeting_app/service/GreetingService.java
+++ src/main/java/greeting_app/service/GreetingService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
     public String greet(String name) {
-        return "Hello, " + name + "!!";
+        return "Hello, " + name + "!";
     }
 }
